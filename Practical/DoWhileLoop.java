7733359171ca62d9f1231c8f18253641d83a13a7
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-05-20 18:00:46 +0530

    Add files via upload

---
+++ Practical/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package Pooja;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=7;
+do {
+System.out.println(i);
+i--;
+}while(i>3);
+	}
+
+}
