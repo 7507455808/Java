28486e951f8b6ab81c3ca9e8d9b1545bb438781a
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-05-20 18:29:40 +0530

    Add files via upload

---
+++ Practical/DoWhileLoopExample1.java
@@ -0,0 +1,19 @@
+//b)	See the example programme for Java do-while loop below.
+//Write the same programme for i = 11 and >3. Show the resulting output.
+
+
+package Pooja;
+
+public class DoWhileLoopExample1 {
+
+	public static void main(String[] args) {
+
+		int i=11;
+		do {
+			System.out.println(i);
+			i--;
+		
+		}while(i>3);
+	}
+
+}
---
+++ Practical/ForLoopExample1.java
@@ -0,0 +1,15 @@
+//a)	See the example programme for Java for-loop below. 
+//Write the same programme for i=10 and i>6. Show the resulting output.
+
+package Pooja;
+
+public class ForLoopExample1 {
+
+	public static void main(String[] args) {
+
+		for(int i=10;i>6;i--) {
+			System.out.println("The value of i is:"+i);
+		}
+	}
+
+}
