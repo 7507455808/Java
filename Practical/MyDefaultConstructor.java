4562a7adb1f9d6360bee9494161ae42e211215df
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-05-20 19:15:24 +0530

    Add files via upload

---
+++ Practical/MyDefaultConstructor.java
@@ -0,0 +1,17 @@
+package Pooja;
+
+public class MyDefaultConstructor {
+	
+	public MyDefaultConstructor() {
+		
+		System.out.println("This is default constructor:");
+	}
+
+	public static void main(String[] args) {
+		
+		MyDefaultConstructor mdc=new MyDefaultConstructor();
+		
+
+	}
+
+}
