cdf25caf0d29ee4ac67fb3d197002b76e556196c
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-05-26 16:15:18 +0530

    Add files via upload

---
+++ Practical/Assignment/PyramidExample.java
@@ -0,0 +1,19 @@
+package Pooja;
+
+public class PyramidExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=5;i++) { //outer loop
+	
+	for(int j=1;j<=i;j++) { //inner loop
+		System.out.print(" * "); //new line
+		
+	}
+
+System.out.println();
+	}
+
+	}
+}
+
