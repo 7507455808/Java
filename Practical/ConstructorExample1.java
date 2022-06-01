4af4d6ab539f4c4864ea87b0916a75e1d3d63678
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-06-01 18:51:04 +0530

    Add files via upload

---
+++ Practical/ConstructorExample1.java
@@ -0,0 +1,31 @@
+package Pooja;
+
+public class ConstructorExample1 {
+	
+	int id;
+	String name;
+	double fees;
+	
+	// creating parameterized constructor
+	public ConstructorExample1(int id, String name, double fees) {
+		this.id = id;
+		this.name = name;
+		this.fees = fees;
+		
+	}
+	// method to display the values
+	void display() {
+		System.out.println(id + " " + name + " " + fees);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// creating object and passing values
+		
+		ConstructorExample1 c = new ConstructorExample1(101,"Chiu",678.90);
+		c.display();
+				
+	}
+
+}
