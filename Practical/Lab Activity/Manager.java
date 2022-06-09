c63192b2dd7cdb2f6f51d791cbbd4c4b6169b9a5
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-06-09 18:13:46 +0530

    Add files via upload

---
+++ Practical/Lab Activity/Manager.java
@@ -0,0 +1,16 @@
+package Lab;
+
+public class Manager extends Employee {
+	
+	public Manager(long Id, String Name, String Address, long phone, double salary) {
+		super(Id, Name, Address, phone);
+		this.employeeId = Id;
+		this.employeeName = Name;
+		this.employeeAddress = Address;
+		this.employeePhone = phone;
+		this.basicSalary = salary;
+	}
+	}
+	
+
+
