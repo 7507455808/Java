6592e3534497a6332f442879e2735c9304b2fa54
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-07-12 16:57:14 +0530

    Add files via upload

---
+++ Practical/Assignment/Dog1.java
@@ -0,0 +1,72 @@
+package Pooja;
+
+public class Dog1 {
+	
+	private String color;
+	private String name;
+	private String bread;
+	private int age;
+
+	
+	
+
+	public static void main(String[] args) {
+	}
+
+
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+	public String getBread() {
+		return bread;
+	}
+
+
+
+
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ Practical/Assignment/EncapsulationEg1.java
@@ -0,0 +1,19 @@
+package Pooja;
+
+
+public class EncapsulationEg1 {
+
+	public static void main(String[] args) {
+
+		Dog1 tommy = new Dog1();
+		
+		//System.out.println(tommy.age);
+		tommy.setAge(15);
+		System.out.println(tommy.getAge());
+		
+		
+		
+		
+		
+	}
+}
