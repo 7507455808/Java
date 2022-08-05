3ea25b224a94edf40807dd2aa3407937f97394bf
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-08-05 15:25:13 +0530

    Add files via upload

---
+++ Practical/HashMapExample.java
@@ -0,0 +1,46 @@
+package CollectionEg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer,String> al = new HashMap<Integer,String>();
+		
+		al.put(1,"mobile"); //adding value with key
+		al.put(2, "bottle");
+		al.put(3, "pen");
+		al.put(4, "pencil");
+		al.put(5, "chair");
+		al.put(6, "box");
+		al.put(null,null); //null pair
+		al.put(8, null); // null value
+		al.put(3, "pen");  //duplicate value
+		al.put(7, "bottle"); //duplicate value
+		al.put(2, "bottle");
+		al.putIfAbsent(9, "marker");
+		al.putIfAbsent(2, "bottle");
+		
+		//traversing map using entrySet
+		for(Map.Entry<Integer,String> m : al.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+		}
+		al.remove(8);
+		al.remove(4,"pencil");
+		al.replace(5, "table chair");
+		
+		System.out.println();
+		for(Map.Entry<Integer, String> m : al.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+			
+		}
+
+
+		
+		
+
+	}
+
+}
