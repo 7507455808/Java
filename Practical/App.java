b4bf3a8e4e2419350c3075494839624e13226e57
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-09-07 17:17:57 +0530

    Add files via upload

---
+++ Practical/App.java
@@ -0,0 +1,14 @@
+package com;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Sample s = new Sample();
+    	s.setId(1);
+    	s.setName("Pooja");
+    	
+    	System.out.println(s.getId() + " " + s.getName());
+    }
+}
