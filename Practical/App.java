788098010e1ca09eb6c66e316eba3ab0a0315a44
Pooja Rathod <105426109+Pooja14061999@users.noreply.github.com>
2022-09-07 17:32:40 +0530

    Add files via upload

--- Practical/App.java
+++ Practical/App.java
@@ -1,14 +1,46 @@
-package com;
+package com.jpaeg;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 
 public class App 
 {
+	@SuppressWarnings("deprecation")
     public static void main( String[] args )
     {
-    	Sample s = new Sample();
-    	s.setId(1);
-    	s.setName("Pooja");
+		//Configuration - activates hibernate framework
+		//configure - read both cfg files & mapping details
+		//buildSessionFactory - from cfg object it takes jdbc information & create a jdbc
+		
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		//open an connection with db & session object perform CRUD
+		Session s = sf.openSession();
+		
+		//save permanently any operation performed on db 
+		Transaction t = s.beginTransaction();
+		
+		//object of persistent class
+		Employee e = new Employee();
+		e.setId(1);
+		e.setFname("Pooja");
+		e.setLname("Rathod");
+		e.setEmail("pooja@gmail.com");
+		e.setPhone(987000321);
+		e.setDesignation("Manager");
+		
+		//save persistent class object e
+		s.save(e);
+		t.commit();
+		
+		System.out.println("inserted successfully");
+		
+		s.close();
+		sf.close();
+		
+		
     	
-    	System.out.println(s.getId() + " " + s.getName());
     }
 }
--- Practical/Employee.java
+++ Practical/Employee.java
@@ -1,21 +1,68 @@
-package CollectionEg;
+package com.jpaeg;
+
+import jakarta.persistence.*;
+
+@Table(name="empdetails")
+@Entity
 
 public class Employee {
 	
-	int empId;
-	String empName;
-	long empPhone;
-	int deptId;
+	@Id
+	private int id;
 	
-	public Employee(int empId, String empName, long empPhone, int deptId) {
-		super();
-		this.empId = empId;
-		this.empName = empName;
-		this.empPhone =empPhone;
-		this.deptId = deptId;
-		
-
-
+	@Column(name="fname", length = 30, nullable = false)	
+	private String fname;
+	
+	@Column(name= "lname", length = 30, nullable = false)
+	private String lname;
+	
+	@Column(name="email", length = 30, unique = true)	
+	private String email;
+	
+	@Column(name="phone", length = 11, nullable = false, unique = true)
+	private long phone;
+	
+	@Column(name="designation", length = 10)
+	private String designation;
+	
+	
+	public int getId() {
+		return id;
 	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
 
 }
